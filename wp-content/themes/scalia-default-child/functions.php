4b26e8a9e27913f74f6c5bf7dce9e941c54e099b
Matt Sobieray <mattsobieray@gmail.com>
2015-06-08 12:40:38 -0600

    db migrate and secondary navigation setup

--- wp-content/themes/scalia-default-child/functions.php
+++ wp-content/themes/scalia-default-child/functions.php
@@ -3,4 +3,10 @@
 add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
 function enqueue_parent_theme_style() {
 	wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css', array('scalia-icons', 'scalia-reset', 'scalia-grid'));
+
 }
+function register_header_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_header_menu' );
+
