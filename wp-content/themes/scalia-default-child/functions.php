918e21292c61872b6ce4c696615b2bfbd5e8ea39
Matt Sobieray <mattsobieray@gmail.com>
2015-06-12 15:56:50 -0600

    added trazact_panel widget

--- wp-content/themes/scalia-default-child/functions.php
+++ wp-content/themes/scalia-default-child/functions.php
@@ -10,3 +10,93 @@ function register_header_menu() {
 }
 add_action( 'init', 'register_header_menu' );
 
+/**
+ * Adds Tranzact_Widget widget.
+ */
+class Tranzact_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'tranzact_widget', // Base ID
+			__( 'Tranzact Panel', 'text_domain' ), // Name
+			array( 'description' => __( 'Tranzact Panel', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		echo __( '<div><div class="drawer"><span><</span></div><h4>Tranzact<span>&reg;</span></h4><p>by zayo</p></div>', 'text_domain' );
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	/*
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+	*/
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	
+
+} // class Tranzact_Widget
+
+// register Tranzact_Widget widget
+function register_tranzact_widget() {
+    register_widget( 'Tranzact_Widget' );
+}
+add_action( 'widgets_init', 'register_tranzact_widget' );
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function tranzact_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Tranzact Panel',
+		'id'            => 'tranzact-panel',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'tranzact_widgets_init' );
+
