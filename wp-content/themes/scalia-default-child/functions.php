c53d4411de4a10d34396afc9da6be9b068865933
Matt Sobieray <mattsobieray@gmail.com>
2015-06-18 14:22:04 -0600

    added answer and expert widgets

--- wp-content/themes/scalia-default-child/functions.php
+++ wp-content/themes/scalia-default-child/functions.php
@@ -76,16 +76,157 @@ class Tranzact_Widget extends WP_Widget {
 
 } // class Tranzact_Widget
 
-// register Tranzact_Widget widget
+// register Expert_Widget widget
 function register_tranzact_widget() {
     register_widget( 'Tranzact_Widget' );
 }
 add_action( 'widgets_init', 'register_tranzact_widget' );
 
+
+
 /**
- * Register our sidebars and widgetized areas.
- *
+ * Adds Expert_Widget widget.
  */
+class Expert_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'expert_widget', // Base ID
+			__( 'Ask An Expert', 'text_domain' ), // Name
+			array( 'description' => __( 'Ask An Expert', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		echo __( '<div class="expert"><p>Ask An Expert</p></div>', 'text_domain' );
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	/*
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+	*/
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	
+
+} // class Expert_Widget
+// register Expert_Widget widget
+function register_expert_widget() {
+    register_widget( 'Expert_Widget' );
+}
+add_action( 'widgets_init', 'register_expert_widget' );
+
+/**
+ * Adds Answer_Widget widget.
+ */
+class Answer_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'answer_widget', // Base ID
+			__( 'Find An Answer', 'text_domain' ), // Name
+			array( 'description' => __( 'Find An Answer', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		echo __( '<div class="expert"><p>Find An Answer</p></div>', 'text_domain' );
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	/*
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+	*/
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	
+
+} // class Answer_Widget
+// register Answert_Widget widget
+function register_answer_widget() {
+    register_widget( 'Answer_Widget' );
+}
+add_action( 'widgets_init', 'register_answer_widget' );
+
+
 function tranzact_widgets_init() {
 
 	register_sidebar( array(
