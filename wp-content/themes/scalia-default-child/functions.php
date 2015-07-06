d90550fd11a3c2e60dc3b4e5d174095f280c899c
Matt Sobieray <mattsobieray@gmail.com>
2015-07-06 16:32:04 -0600

    added pardot widget and style updates to for the top navigation

--- wp-content/themes/scalia-default-child/functions.php
+++ wp-content/themes/scalia-default-child/functions.php
@@ -241,3 +241,74 @@ function tranzact_widgets_init() {
 }
 add_action( 'widgets_init', 'tranzact_widgets_init' );
 
+/**
+ * Adds Pardot_Form widget.
+ */
+class Pardot_Form extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'pardot_form', // Base ID
+			__( 'Pardot Form', 'text_domain' ), // Name
+			array( 'description' => __( 'Pardot Form', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		echo __( '<iframe src="http://go.zayo.com/l/48602/2014-07-03/7i" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>', 'text_domain' );
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	/*
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+	*/
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	
+
+} // class Tranzact_Widget
+
+// register Expert_Widget widget
+function register_pardot_form() {
+    register_widget( 'Pardot_Form' );
+}
+add_action( 'widgets_init', 'register_pardot_form' );
--- wp-content/themes/scalia-default-child/header.php
+++ wp-content/themes/scalia-default-child/header.php
@@ -50,97 +50,98 @@
 	<?php if(!scalia_get_option('disable_scroll_top_button')) : ?>
 		<a href="#page" class="scroll-top-button"></a>
 	<?php endif; ?>
+	<div class="nav-gradient">
+		<?php if(scalia_get_option('top_area_style')) : ?>
+			<div id="top-area" class="top-area top-area-style-<?php echo esc_attr(scalia_get_option('top_area_style')); ?>">
+				<div class="container">
+					<div class="top-area-items clearfix">
+						<!-- <?php if(scalia_get_option('top_area_socials')) : ?>
+							<div class="top-area-socials"><?php scalia_print_socials(); ?></div>
+						<?php endif; ?> -->
+						<?php if(scalia_get_option('top_area_style') == 1) : ?>
+							<?php if(scalia_get_option('top_area_search')) : ?>
+								<div class="top-area-search"><?php scalia_toparea_search_form(); ?></div>
+							<?php endif; ?>
+							<?php if(scalia_get_option('top_area_contacts')) : ?>
+								<div class="top-area-contacts"><?php echo scalia_contacts(true); ?></div>
+							<?php endif; ?>
+								<!-- Theme Customization: adding navigation to the header -->
+							<?php if(has_nav_menu('header-menu')) : ?>
+							<nav id="secondary-navigation" class="site-navigation primary-navigation" role="navigation">
+								<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_id' => 'secondary-menu', 'menu_class' => 'nav-menu dl-menu styled no-responsive', 'container' => false, 'walker' => new Scalia_Mega_Menu_Walker)); ?>
+							</nav>
+						<?php endif; ?>
+						<?php else : ?>
+							<?php if(scalia_get_option('top_area_contacts')) : ?>
+								<div class="top-area-contacts"><?php echo scalia_contacts(true); ?></div>
+							<?php endif; ?>
+							<?php if(scalia_get_option('top_area_search')) : ?>
+								<div class="top-area-search"><?php scalia_toparea_search_form(); ?></div>
+							<?php endif; ?>
+					
+						<?php endif; ?>
+						
+					</div>
+				</div>
+			</div>
+		<?php endif; ?>
 
-	<?php if(scalia_get_option('top_area_style')) : ?>
-		<div id="top-area" class="top-area top-area-style-<?php echo esc_attr(scalia_get_option('top_area_style')); ?>">
+		<header id="site-header" class="site-header<?php echo scalia_get_option('disable_fixed_header') ? '' : ' animated-header'; ?><?php echo scalia_get_option('header_on_slideshow') ? ' header-on-slideshow' : ''; ?>" role="banner">
+
 			<div class="container">
-				<div class="top-area-items clearfix">
-					<!-- <?php if(scalia_get_option('top_area_socials')) : ?>
-						<div class="top-area-socials"><?php scalia_print_socials(); ?></div>
-					<?php endif; ?> -->
-					<?php if(scalia_get_option('top_area_style') == 1) : ?>
-						<?php if(scalia_get_option('top_area_search')) : ?>
-							<div class="top-area-search"><?php scalia_toparea_search_form(); ?></div>
-						<?php endif; ?>
-						<?php if(scalia_get_option('top_area_contacts')) : ?>
-							<div class="top-area-contacts"><?php echo scalia_contacts(true); ?></div>
-						<?php endif; ?>
-							<!-- Theme Customization: adding navigation to the header -->
-						<?php if(has_nav_menu('header-menu')) : ?>
-						<nav id="secondary-navigation" class="site-navigation primary-navigation" role="navigation">
-							<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_id' => 'secondary-menu', 'menu_class' => 'nav-menu dl-menu styled no-responsive', 'container' => false, 'walker' => new Scalia_Mega_Menu_Walker)); ?>
-						</nav>
+				<div class="header-main logo-position-<?php echo esc_attr(scalia_get_option('logo_position', 'left')); ?>">
+					<?php if(scalia_get_option('logo_position', 'left') != 'right') : ?>
+					<div class="site-title">
+						<h1>
+							<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+								<?php if(scalia_get_option('logo')) : ?>
+									<span class="logo logo-1x"><img src="<?php echo esc_url(scalia_get_option('logo')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo')); ?>" class="small" alt=""><?php endif; ?></span>
+									<?php if(scalia_get_option('logo_2x')) : ?>
+										<span class="logo logo-2x"><img src="<?php echo esc_url(scalia_get_option('logo_2x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_2x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_2x')); ?>" class="small" alt=""><?php endif; ?></span>
+									<?php endif; ?>
+									<?php if(scalia_get_option('logo_3x')) : ?>
+										<span class="logo logo-3x"><img src="<?php echo esc_url(scalia_get_option('logo_3x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_3x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_3x')); ?>" class="small" alt=""><?php endif; ?></span>
+									<?php endif; ?>
+								<?php else : ?>
+									<?php bloginfo('name'); ?>
+								<?php endif; ?>
+							</a>
+						</h1>
+					</div>
+					<?php if(has_nav_menu('primary')) : ?>
+					<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+						<button class="menu-toggle dl-trigger"><?php _e('Primary Menu', 'codeus'); ?></button>
+						<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav-menu dl-menu styled no-responsive', 'container' => false, 'walker' => new Scalia_Mega_Menu_Walker)); ?>
+					</nav>
 					<?php endif; ?>
 					<?php else : ?>
-						<?php if(scalia_get_option('top_area_contacts')) : ?>
-							<div class="top-area-contacts"><?php echo scalia_contacts(true); ?></div>
-						<?php endif; ?>
-						<?php if(scalia_get_option('top_area_search')) : ?>
-							<div class="top-area-search"><?php scalia_toparea_search_form(); ?></div>
-						<?php endif; ?>
-				
+					<?php if(has_nav_menu('primary')) : ?>
+					<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+						<button class="menu-toggle dl-trigger"><?php _e('Primary Menu', 'codeus'); ?></button>
+						<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav-menu dl-menu styled no-responsive', 'container' => false, 'walker' => new Scalia_Mega_Menu_Walker)); ?>
+					</nav>
 					<?php endif; ?>
-					
+					<div class="site-title">
+						<h1>
+							<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+								<?php if(scalia_get_option('logo')) : ?>
+									<span class="logo logo-1x"><img src="<?php echo esc_url(scalia_get_option('logo')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo')); ?>" class="small" alt=""><?php endif; ?></span>
+									<?php if(scalia_get_option('logo_2x')) : ?>
+										<span class="logo logo-2x"><img src="<?php echo esc_url(scalia_get_option('logo_2x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_2x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_2x')); ?>" class="small" alt=""><?php endif; ?></span>
+									<?php endif; ?>
+									<?php if(scalia_get_option('logo_3x')) : ?>
+										<span class="logo logo-3x"><img src="<?php echo esc_url(scalia_get_option('logo_3x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_3x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_3x')); ?>" class="small" alt=""><?php endif; ?></span>
+									<?php endif; ?>
+								<?php else : ?>
+									<?php bloginfo('name'); ?>
+								<?php endif; ?>
+							</a>
+						</h1>
+					</div>
+					<?php endif; ?>
 				</div>
 			</div>
-		</div>
-	<?php endif; ?>
-
-	<header id="site-header" class="site-header<?php echo scalia_get_option('disable_fixed_header') ? '' : ' animated-header'; ?><?php echo scalia_get_option('header_on_slideshow') ? ' header-on-slideshow' : ''; ?>" role="banner">
-
-		<div class="container">
-			<div class="header-main logo-position-<?php echo esc_attr(scalia_get_option('logo_position', 'left')); ?>">
-				<?php if(scalia_get_option('logo_position', 'left') != 'right') : ?>
-				<div class="site-title">
-					<h1>
-						<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-							<?php if(scalia_get_option('logo')) : ?>
-								<span class="logo logo-1x"><img src="<?php echo esc_url(scalia_get_option('logo')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo')); ?>" class="small" alt=""><?php endif; ?></span>
-								<?php if(scalia_get_option('logo_2x')) : ?>
-									<span class="logo logo-2x"><img src="<?php echo esc_url(scalia_get_option('logo_2x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_2x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_2x')); ?>" class="small" alt=""><?php endif; ?></span>
-								<?php endif; ?>
-								<?php if(scalia_get_option('logo_3x')) : ?>
-									<span class="logo logo-3x"><img src="<?php echo esc_url(scalia_get_option('logo_3x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_3x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_3x')); ?>" class="small" alt=""><?php endif; ?></span>
-								<?php endif; ?>
-							<?php else : ?>
-								<?php bloginfo('name'); ?>
-							<?php endif; ?>
-						</a>
-					</h1>
-				</div>
-				<?php if(has_nav_menu('primary')) : ?>
-				<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-					<button class="menu-toggle dl-trigger"><?php _e('Primary Menu', 'codeus'); ?></button>
-					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav-menu dl-menu styled no-responsive', 'container' => false, 'walker' => new Scalia_Mega_Menu_Walker)); ?>
-				</nav>
-				<?php endif; ?>
-				<?php else : ?>
-				<?php if(has_nav_menu('primary')) : ?>
-				<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-					<button class="menu-toggle dl-trigger"><?php _e('Primary Menu', 'codeus'); ?></button>
-					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav-menu dl-menu styled no-responsive', 'container' => false, 'walker' => new Scalia_Mega_Menu_Walker)); ?>
-				</nav>
-				<?php endif; ?>
-				<div class="site-title">
-					<h1>
-						<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-							<?php if(scalia_get_option('logo')) : ?>
-								<span class="logo logo-1x"><img src="<?php echo esc_url(scalia_get_option('logo')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo')); ?>" class="small" alt=""><?php endif; ?></span>
-								<?php if(scalia_get_option('logo_2x')) : ?>
-									<span class="logo logo-2x"><img src="<?php echo esc_url(scalia_get_option('logo_2x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_2x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_2x')); ?>" class="small" alt=""><?php endif; ?></span>
-								<?php endif; ?>
-								<?php if(scalia_get_option('logo_3x')) : ?>
-									<span class="logo logo-3x"><img src="<?php echo esc_url(scalia_get_option('logo_3x')); ?>" class="default" alt=""><?php if(scalia_get_option('small_logo_3x')) : ?><img src="<?php echo esc_url(scalia_get_option('small_logo_3x')); ?>" class="small" alt=""><?php endif; ?></span>
-								<?php endif; ?>
-							<?php else : ?>
-								<?php bloginfo('name'); ?>
-							<?php endif; ?>
-						</a>
-					</h1>
-				</div>
-				<?php endif; ?>
-			</div>
-		</div>
-	</header><!-- #site-header -->
+		</header><!-- #site-header -->
+	</div><!-- .nav-gradient -->
 
 	<div id="main" class="site-main">
